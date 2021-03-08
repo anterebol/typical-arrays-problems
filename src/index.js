31b50c4b3788ae7cdfd04b72b8e70809ebe9f35e
unknown <anterebol@gmail.com>
2021-03-08 16:59:07 +0300

    fead: add index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,60 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arg) {
+  if (typeof(arg) === 'object') {
+  let value = arg[0];
+    if (typeof(value) === 'number') {
+  for (let i = 1; i < arg.length; i++) {
+    if (value > arg[i]) {
+      value = arg[i];
+    }
+  } 
+  return value; 
+  } 
+  else {
+    return 0;
+  }
+}
+  else {
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arg) {
+  if (typeof(arg) === 'object') {
+  let value = arg[0];
+    if (typeof(value) === 'number') {
+  for (let i = 1; i < arg.length; i++) {
+    if (value < arg[i]) {
+      value = arg[i];
+    }
+  } 
+  return value; 
+  } 
+  else {
+    return 0;
+  }
+}
+  else {
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arg) {
+  if (typeof(arg) === 'object') {
+  let value = arg[0];
+    if (typeof(value) === 'number') {
+  for (let i = 1; i < arg.length; i++) {
+    value = value + arg[i];
+  }
+   value = value / arg.length; 
+   return value; 
+  } 
+    else {
+    return 0;
+  }
+  }
+    else {
+    return 0;
+  }
 }
+
